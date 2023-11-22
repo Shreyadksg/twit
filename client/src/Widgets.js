39c05a20a7426e16b6daa35b70ae983822603a09
Sneha Kumari <snehakr654@gmail.com>
2023-11-22 22:56:14 +0530

    update
    Co-Authored-By: sneha-kr20 <97392648+sneha-kr20@users.noreply.github.com>
    Co-Authored-By: the_1111 <97393428+Shreyadksg@users.noreply.github.com>

--- client/src/Widgets.js
+++ client/src/Widgets.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./widgets.css";
+
+import {
+  TwitterTimeLineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+ } from "react-twitter-embed";
+
+ import SearchIcon from "@material-ui/icons/Search";
 
 const Widgets = () => {
   return (
-    <div>Widgets</div>
+    <div className="widgets">
+      <div className ="widgets_input">
+        <SearchIcon className ="widgets_searchIcon"/>
+        <input placeholder="Search Twitter" type="text"/>
+
+      </div>
+
+    </div>
   )
 }
 
