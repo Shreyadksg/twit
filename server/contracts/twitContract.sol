f2720861949627c1f9955ab26b781a26218a3f41
Aditi Shandilya <122087467+aditishandily@users.noreply.github.com>
2023-11-22 19:39:11 +0530

    update

--- server/contracts/twitContract.sol
+++ server/contracts/twitContract.sol
@@ -14,11 +14,14 @@ contract twitContract {
         bool isDeleted;
     }
 
+    //data strucure to privately store tweet IDs.
     Tweet[] private tweets;
 
     //Mapping of Tweet id to the wallet address of the user
+    //helps to find out twitter IDs owned by respective person
     mapping(uint256 => address) tweetToOwner;
 
+    //function to add a new tweet
     function addTweet(string memory tweetText, bool isDeleted) external {
         uint tweetId = tweets.length;
         tweets.push(Tweet(tweetId,msg.sender,tweetText,isDeleted));
@@ -30,16 +33,16 @@ contract twitContract {
     function getAllTweets() external view returns (Tweet[] memory){
         Tweet[] memory temporary = new Tweet[](tweets.length);
 
-        uint counter=0;
+        uint count=0;
         for(uint i=0;i<tweets.length;i++){
             if(tweets[i].isDeleted==false){
-                temporary[counter]=tweets[i];
-                counter++;
+                temporary[count]=tweets[i];
+                count++;
             }
         }
 
-        Tweet[] memory result=new Tweet[](counter);
-        for(uint i=0;i<counter;i++){
+        Tweet[] memory result=new Tweet[](count);
+        for(uint i=0;i<count;i++){
             result[i]=temporary[i];
         }
 
